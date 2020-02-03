19f573017988e368c685112a93aa261f02fbecda
UlrichMabou <ulrichmabou@gmail.com>
2020-02-04 01:13:03 +0300

    use the story plan to progress through the story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,8 @@
 import 'story.dart';
 
 class StoryBrain {
+  int storyNumber = 0;
+
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -46,19 +48,29 @@ class StoryBrain {
   }
 
   void nextStory(int choiceNumber) {
-    print('choice $choiceNumber selected');
+    if (storyNumber == 0 && choiceNumber == 1) {
+      storyNumber = 2;
+    } else {
+      storyNumber = 1;
+    }
+
+    if (storyNumber == 1 && choiceNumber == 1) {
+      storyNumber = 2;
+    } else {
+      storyNumber = 3;
+    }
+
+    if (storyNumber == 2 && choiceNumber == 1) {
+      storyNumber = 5;
+    } else {
+      storyNumber = 4;
+    }
   }
 }
-
-int storyNumber = 0;
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
